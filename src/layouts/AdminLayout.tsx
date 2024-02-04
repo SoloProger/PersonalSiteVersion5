bb2d88f5cbf25f538b3fe5beb545bd98ba017335
serafimow <aserafimow@gmail.com>
2024-02-04 12:49:28 +0300

    changes

---
+++ src/layouts/AdminLayout.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "../admin/components/sidebar/Sidebar.tsx";
+
+function AdminLayout(props) {
+    return (
+        <div className="admin">
+            <Sidebar/>
+        </div>
+    );
+}
+
+export default AdminLayout;
--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import MainLayout from './layouts/MainLayout.tsx';
 import MainPage from './pages/MainPage.tsx';
 import AboutPage from './pages/AboutPage.tsx';
 import PortfolioPage from './pages/PortfolioPage.tsx';
+import AdminLayout from "./layouts/AdminLayout.tsx";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: '/admin',
+    element: <AdminLayout />
+  }
 ]);
 
 export default router;
